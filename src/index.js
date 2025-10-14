91fcd9865ed3e8a5de16f49b7c9950127256a6ef
zephyrus1009 <136441242+zephyrus1009@users.noreply.github.com>
2025-10-14 14:35:44 +0700

    refactor: use Strapi lifecycle module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-const strapi = require('@strapi/strapi');
+'use strict';
 
-strapi().start();
+module.exports = {
+  async register(/*{ strapi }*/) {
+    // Additional register logic can be added here if needed.
+  },
 
+  async bootstrap(/*{ strapi }*/) {
+    // Application bootstrap logic can be added here if needed.
+  },
+
+  async destroy(/*{ strapi }*/) {
+    // Clean-up logic can be added here if needed.
+  },
+};
